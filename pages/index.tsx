f212c299654091d017c7aaae0846e85b794a04de
Alex Bussey <awbussey83@gmail.com>
2021-01-25 18:51:29 -0500

    Add deps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+import "tailwindcss/tailwind.css";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
@@ -10,6 +12,6 @@ const IndexPage = () => (
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
